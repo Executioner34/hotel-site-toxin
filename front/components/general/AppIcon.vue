89dad331cb9b97c876c5ac403e1f8910e18be74d
Gorlenko Alexander <goral92@inbox.ru>
2023-05-03 12:56:39 +0300

    feat: добавляет svg-sprite в проект

---
+++ front/components/general/AppIcon.vue
@@ -0,0 +1,18 @@
+<template>
+  <svg-icon :name="props.name" :width="props.width" :height="props.height" />
+</template>
+
+<script setup lang="ts">
+interface IProps {
+  name: string;
+  width?: string;
+  height?: string;
+}
+
+const props = withDefaults(defineProps<IProps>(), {
+  width: "20px",
+  height: "20px",
+});
+</script>
+
+<style scoped lang="scss"></style>
