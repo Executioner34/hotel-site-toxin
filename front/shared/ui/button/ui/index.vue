6f0acee3d9a5d4aa6b6d5c903dce37cad7af188b
Alexander Gorlenko <60706892+Executioner34@users.noreply.github.com>
2023-09-18 19:41:15 +0300

    feat: реализует компонент кнопки

---
+++ front/shared/ui/button/ui/index.vue
@@ -0,0 +1,108 @@
+<script setup lang="ts">
+interface IProps {
+  color?: "secondary" | "primary";
+  size?: "medium" | "large";
+}
+
+const props = withDefaults(defineProps<IProps>(), {
+  color: "secondary",
+  size: "medium",
+});
+
+const classList = computed(() => {
+  const list = ["button"];
+  if (props.color === "primary" || props.size === "large") list.push("_fill");
+  if (props.size === "large") list.push("_large");
+  return list;
+});
+
+const isLarge = computed(() => props.size === "large");
+</script>
+
+<template>
+  <button :class="classList">
+    <span class="button__text">
+      <slot></slot>
+      <Icon
+        v-if="isLarge"
+        name="ic:baseline-arrow-forward"
+        size="24px"
+        class="button__icon"
+      ></Icon>
+    </span>
+  </button>
+</template>
+
+<style scoped lang="scss">
+.button {
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  max-width: 99px;
+  width: 100%;
+  height: 44px;
+  padding: 14px 20px 15px;
+  background-clip: padding-box;
+  background-color: $white;
+  border: 2px solid transparent;
+  border-radius: 44px;
+
+  &::before {
+    content: "";
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    z-index: -1;
+    margin: -2px;
+    border-radius: inherit;
+    background: linear-gradient(to bottom, $purpleColor, #8ba4f9);
+  }
+
+  &:active::before,
+  &:target::before,
+  &:disabled::before {
+    opacity: 0.5;
+  }
+
+  &__text {
+    @include h3Text;
+    white-space: nowrap;
+    color: $purpleColor;
+  }
+
+  &._fill {
+    background: linear-gradient(to bottom, $purpleColor, #8ba4f9);
+    border: none;
+
+    &::before {
+      display: none;
+    }
+
+    &:target,
+    &:active {
+      opacity: 0.5;
+    }
+
+    .button__text {
+      color: $white;
+    }
+  }
+
+  &._fill:disabled {
+    opacity: 0.5;
+  }
+
+  &._large {
+    max-width: 320px;
+  }
+
+  &__icon {
+    position: absolute;
+    top: 10px;
+    right: 10px;
+  }
+}
+</style>
