ef25cd5ec1f6c0d1c8bea969982a2a221b73a675
Alexander Gorlenko <60706892+Executioner34@users.noreply.github.com>
2023-09-20 19:07:13 +0300

    feat: реализует компонент рейтинга

---
+++ front/shared/ui/rating/ui/index.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+interface IProps {
+  stars?: number;
+}
+
+const props = withDefaults(defineProps<IProps>(), {
+  stars: 0,
+});
+
+const classIcon = (value: number) => {
+  return value <= props.stars ? "_star" : "_star-border";
+};
+</script>
+
+<template>
+  <div class="rating">
+    <div
+      v-for="(star, ind) in 5"
+      :key="`star-${ind}`"
+      :class="['rating__star', classIcon(ind + 1)]"
+    ></div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.rating {
+  display: flex;
+
+  &__star {
+    width: 24px;
+    height: 24px;
+    background-repeat: no-repeat;
+    background-position: center center;
+
+    &._star {
+      background-image: url("~/app/ico/star.svg");
+    }
+
+    &._star-border {
+      background-image: url("~/app/ico/star-border.svg");
+    }
+  }
+}
+</style>
