1aebe6cb21dd4f00646422878475f88971590892
Alexander Gorlenko <60706892+Executioner34@users.noreply.github.com>
2023-10-22 16:15:00 +0300

    feat: реализует компонент Card

---
+++ front/shared/ui/card/ui/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="card">
+    <slot name="header"></slot>
+    <slot></slot>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="scss" scoped>
+.card {
+  display: flex;
+  flex-direction: column;
+  min-width: 320px;
+  width: 100%;
+  padding: 40px 30px 30px;
+  border: 1px solid #0000001f;
+  border-radius: 4px;
+  box-shadow: 0px 0px 25px 0px rgba(0, 0, 0, 0.2);
+}
+</style>
