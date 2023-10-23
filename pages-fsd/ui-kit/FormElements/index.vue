694d3d64f677fb7bf8dd8c887d9de8f3757e6b00
Alexander Gorlenko <60706892+Executioner34@users.noreply.github.com>
2023-10-23 10:10:31 +0300

    feat: удаляет бекенд из репозитория

---
+++ pages-fsd/ui-kit/FormElements/index.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { Card } from "@/shared/ui/card";
+import { Input } from "@/shared/ui/input";
+</script>
+
+<template>
+  <div class="form-elements-page">
+    <Card class="card">
+      <template #header>
+        <h2 class="title">Title</h2>
+      </template>
+      <Input id="input" type="text" placeholder="Имя"></Input>
+      <Input id="input" type="text" placeholder="Фамилия"></Input>
+    </Card>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.form-elements-page {
+  .column {
+    display: flex;
+    flex-direction: column;
+  }
+
+  .card {
+    width: 380px;
+  }
+}
+</style>
